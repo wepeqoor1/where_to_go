4442d1ca8a6be9bf71e36c3651a76c22cef30c92
wepeqoor1 <hoopeeeee@gmail.com>
2023-05-08 18:38:32 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,9 +1,37 @@
+import json
+from typing import Iterator
+
 from django.shortcuts import render
 
+from .models import Place
 
-def main_page(request):
-    return render(request, 'main_page.html')
+
+def get_places():
+    places = Place.objects.all()
+
+    for place in places:
+        data = {
+            "type": place.type,
+            "geometry": {
+                "type": place.geometry_type,
+                "coordinates": [place.latitude, place.longitude]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.place_id,
+                "detailsUrl": place.details_url
+            }
+        }
+        yield data
 
 
 def main_page_map(request):
-    return render(request, 'main_page_map.html')
+    places: Iterator[dict] = get_places()
+    places_geo = {
+      'type': 'FeatureCollection',
+      'features': [
+          dict(place) for place in places
+      ]
+    }
+
+    return render(request, 'main_page_map.html', context={'places_geo': places_geo})
