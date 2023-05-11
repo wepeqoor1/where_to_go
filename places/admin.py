0136c25d37b375350b996f2524e5c9a40d053727
wepeqoor1 <hoopeeeee@gmail.com>
2023-05-11 21:46:08 +0300

    Show image preview in Places and Images admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from .models import Place, Image
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('image', 'image_preview', 'position')
+    readonly_fields = ('image_preview',)
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    list_display = ('image', 'image_preview', 'position')
+    readonly_fields = ('image_preview',)
 
 
 @admin.register(Place)
--- places/models.py
+++ places/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import format_html
 
 
 class Place(models.Model):
@@ -24,6 +25,11 @@ class Image(models.Model):
     position = models.IntegerField(blank=True, null=True, verbose_name='Позиция картинки')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, null=True, verbose_name='Дата обновления')
+
+    def image_preview(self):
+        return format_html('<img src="{}" width="100" />'.format(self.image.url))
+
+    image_preview.short_description = 'Изображение'
 
     def __str__(self):
         return f'{self.id}. {self.place.title}'
